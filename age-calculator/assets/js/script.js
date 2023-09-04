542c144284a89ac126e8a641b2ce8da11896291b
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-09-04 16:52:22 -0300

    criando-calc

---
+++ age-calculator/assets/js/script.js
@@ -0,0 +1,10 @@
+const day = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+const dayTxt = document.getElementById('day-txt');
+const monthTxt = document.getElementById('month-txt');
+const yearTxt = document.getElementById('year-txt');
+
+function calcular() {
+    const date = new Date
+}
