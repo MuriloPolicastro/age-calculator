120b03e280228adb5a3176e605158942183e2161
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-09-06 07:57:22 -0300

    att

--- age-calculator/assets/js/script.js
+++ age-calculator/assets/js/script.js
@@ -1,10 +1,28 @@
-const day = document.getElementById('day');
-const month = document.getElementById('month');
-const year = document.getElementById('year');
-const dayTxt = document.getElementById('day-txt');
-const monthTxt = document.getElementById('month-txt');
-const yearTxt = document.getElementById('year-txt');
+const dayTxt = document.querySelector('#days-txt');
+const monthTxt = document.querySelector('#month-txt');
+const yearTxt = document.querySelector('#year-txt');
+const error = document.querySelector('#error')
 
 function calcular() {
-    const date = new Date();
+    if (dayTxt = '' || dayTxt < 0) return error.innerHTML = 'Dia inválido!';
+
+    if (dayTxt > 0) {
+        const day = document.getElementById('day').value;
+        const month = document.getElementById('month').value;
+        const year = document.getElementById('year').value;
+        const date = `${year}-${month}-${day}`;
+
+        const dateNow = new Date();
+        const dayNow = dateNow.getDate();
+        const monthNow = dateNow.getMonth();
+        const yearNow = dateNow.getFullYear();
+
+        const diffDay = (dayNow+30) - day;
+        const diffMonth = (monthNow+1) - month;
+        const diffYear = yearNow - year;
+
+        dayTxt.innerHTML = diffDay,
+        monthTxt.innerHTML = diffMonth,
+        yearTxt.innerHTML = diffYear;
+    }
 }
