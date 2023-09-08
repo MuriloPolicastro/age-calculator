6c95a2930fefe83cf0c09accaf9484555484c028
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-09-08 16:55:15 -0300

    update script

--- age-calculator/assets/js/script.js
+++ age-calculator/assets/js/script.js
@@ -12,22 +12,46 @@ const monthTxt = document.querySelector('#month-txt');
 const yearTxt = document.querySelector('#year-txt');
 const error = document.querySelector('#error')
 
-function calcular() {
+const dateNow = new Date();
+const dayNow = dateNow.getDate();
+const monthNow = dateNow.getMonth();
+const yearNow = dateNow.getFullYear();
+
+function calcular(day, month, year) {
+    let diffDay = (dayNow+30) - day;
+    let diffMonth = (monthNow+1) - month;
+    let diffYear = yearNow - year;
+
+    if (diffDay >= 30 ) {
+        dayTxt.innerHTML = diffDay - 30;
+    } else {
+        dayTxt.innerHTML = diffDay;
+    }
+    if (dayTxt == dayNow && month == (monthNow+1)) {
+        monthTxt.innerHTML = 11;
+    } else if (month == (monthNow+1)) {
+        monthTxt.innerHTML = 0;
+        diffYear++;
+    } else if (diffMonth < 0) {
+        monthTxt.innerHTML = 12 + diffMonth;
+    } else {
+        monthTxt.innerHTML = diffMonth;
+    }
+
+    yearTxt.innerHTML = diffYear;
+}
+
+function executar() {
     const day = document.getElementById('day').value;
     const month = document.getElementById('month').value;
     const year = document.getElementById('year').value;
-    const date = `${year}-${month}-${day}`;
 
-    const dateNow = new Date();
-    const dayNow = dateNow.getDate();
-    const monthNow = dateNow.getMonth();
-    const yearNow = dateNow.getFullYear();
-
-    const diffDay = (dayNow+30) - day;
-    const diffMonth = (monthNow+1) - month;
-    const diffYear = yearNow - year;
-
-    dayTxt.innerHTML = diffDay,
-    monthTxt.innerHTML = diffMonth,
-    yearTxt.innerHTML = diffYear;
+    if (day <= 0 || month <= 0 || year <=0) return window.alert ('Data inválida');
+    if (day > 31) return window.alert ('Dia inválido');
+    if (month > 12) return window.alert ('Mês inválido');
+    if (year > yearNow) {
+        window.alert ('Ano inválido');
+    } else {
+        calcular(day, month, year);
+    }
 }
